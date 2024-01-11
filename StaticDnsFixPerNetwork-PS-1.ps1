c2097e6d6e581c3e6d19a871bc620f9fc718cbe8
Timothy Burnsed <143354694+tburnsed@users.noreply.github.com>
2024-01-11 12:55:45 -0500

    Add files via upload

---
+++ StaticDnsFixPerNetwork-PS-1.ps1
@@ -0,0 +1,12 @@
+﻿$Interfaces = 'hklm:\SYSTEM\ControlSet001\Services\Tcpip\Parameters\Interfaces\'
+
+Get-ChildItem $Interfaces | Where-Object {
+    #Only where it contants a key already called "NameServer"
+    (Get-ItemProperty -Path $_.PSPath -Name '*NameServer*' -ErrorAction SilentlyContinue)
+   
+} |
+
+ForEach-Object {
+     #Changes each name server to blank
+     Set-ItemProperty -Path $_.PSPath -Name 'NameServer' -Type 'String' -Value ''
+}
